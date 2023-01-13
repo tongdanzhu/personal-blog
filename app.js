b19171d1cc2551239c1b07d63940702c0509d68d
tongdanzhu <tongdan6509@gmail.com>
2023-01-12 21:55:19 -0500

    finish functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash"); // load the full build of lodash
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,7 +21,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.render("home", {
+  res.render("home", {
     homeStartingContent: homeStartingContent,
     posts: posts
   });
@@ -43,34 +44,35 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-
+  const tempPostTitles = [];
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
 
-  posts.push(post); // [[title, content], [title, content], ...]
+  posts.forEach(function(post){
+    tempPostTitles.push(post.title);
+  });
+
+  if (!tempPostTitles.includes(post.title)){
+    posts.push(post); // [[title, content], [title, content], ...  ]
+  }
 
   res.redirect("/");
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function(req, res) {
 
-  const requestTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName); // format requested title
+  // console.log(requestedTitle);
 
-  posts.forEach(function(post){
-    const storedTitle = post.title;
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
 
-    if (storedTitle === requestTitle){
-      console.log("Find Match!");
+    if (requestedTitle === storedTitle) {
+      res.render("post", {post: post});
     }
-    else{
-      console.log("Not found");
-    }
-  })
-
-
-
+  });
 });
 
 app.listen(3000, function() {
